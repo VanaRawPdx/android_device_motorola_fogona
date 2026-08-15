8dc4e6ccff706bdafd634fe01541e2048731fad9
Van Kessler <shlyftnick71@gmail.com>
2026-08-15 13:09:13 -0700

    Add TWRP configuration for Motorola Moto G Play 2024

---
+++ .github/workflows/twrp_fogona.mk
@@ -0,0 +1,18 @@
+# Inherit from AOSP / TWRP generic configurations
+$(call inherit-product,$(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product,$(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product,$(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit TWRP extra build features
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier
+PRODUCT_DEVICE := fogona
+PRODUCT_NAME := twrp_fogona
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G Play 2024
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=fogona \
+    PRODUCT_NAME=fogona
